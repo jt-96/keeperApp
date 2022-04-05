0920e87785fe6e7f102d6a32262afba7bbc4dbf1
jt-96 <jetorrico@outlook.com>
2022-04-05 14:52:33 -0300

    typo

--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Note(props){
 
     return (
-        <div className='note'>
+        <div className="note">
             <h1>{props.entry.title}</h1>
             <p>{props.entry.content}</p>
             <button>DELETE</button>
