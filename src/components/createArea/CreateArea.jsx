8c484d11957170d5b99a5cf5602aae45712dd03d
jt-96 <jetorrico@outlook.com>
2022-04-05 14:51:55 -0300

    added new component createArea

---
+++ src/components/createArea/CreateArea.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function CreateArea(){
+
+    return (
+        <div>
+            <form>
+                <input type="text" name="title" placeholder='Title' />
+                <textarea name="content" placeholder="Take a note" rows={3}></textarea>
+                <button>Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default CreateArea;
