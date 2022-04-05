aa07c2b2cb0b79c12f393b18eee297886a71c421
jt-96 <jetorrico@outlook.com>
2022-04-05 15:45:00 -0300

    added add-note functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import React, { useState } from 'react';
 import CreateArea from './components/createArea/CreateArea';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Note from './components/note/Note';
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((prevValue) => {
+      return [...prevValue, note];
+    });
+  }
+
   return (
     <div className="App">
       <Header />
-      <CreateArea />
-      <Note title="test title" content="test content"/>
+      <CreateArea onAdd={addNote}/>
+      {
+        notes.map((note, index) => (
+
+          <Note key={index} title={note.title} content={note.content} />
+        ))
+      }
       <Footer />
     </div>
   );
--- src/components/createArea/CreateArea.jsx
+++ src/components/createArea/CreateArea.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function CreateArea(){
+function CreateArea(props) {
+
+    const [newNote, setNewNote] = useState({
+        title: "",
+        content: ""
+    });
+
+
+    function handleChange(event) {
+
+        const { name, value } = event.target;
+
+        setNewNote((prevValue) => {
+            return {
+                ...prevValue,
+                [name]: value
+            }
+        });
+    }
 
     return (
         <div>
             <form>
-                <input type="text" name="title" placeholder='Title' />
-                <textarea name="content" placeholder="Take a note" rows={3}></textarea>
-                <button>Add</button>
-            </form>
-        </div>
+                <input onChange={handleChange} type="text" name="title" placeholder='Title' value={newNote.title}/>
+                <textarea onChange={handleChange} name="content" placeholder="Take a note" rows={3} value={newNote.content}></textarea>
+                <button type='button' onClick={() => {
+                    props.onAdd(newNote);
+                    setNewNote({title: "", content: ""});
+                }}>Add</button>
+        </form>
+        </div >
     )
 }
 
